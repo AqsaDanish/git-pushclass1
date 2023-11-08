410facc950d7aba3b3bedfe0815799114d7ba0f0
unknown <aqsasameerkhan5.com>
2023-11-08 15:55:09 +0500

    first commit

---
+++ new-file.py
@@ -0,0 +1,12 @@
+class item():
+    def_init__(name,price,quantity):
+    self.name = Name
+    self.price = price
+    self.quantity = quantity
+
+def printinformation(self):
+    print("name: ",self.name)
+    print("price: ",self.price) 
+    print("quantity: ",self.quantity)
+    
+       
